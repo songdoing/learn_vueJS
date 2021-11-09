09807182a7e8995ae238488bd56688a8ab7e69ea
wonjinsong <msmk22239@gmail.com>
2021-11-09 13:51:14 -0500

    event emit

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <app-header v-bind:propsdata="str"></app-header>
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>
   </div>
 </template>
 
@@ -31,6 +31,11 @@ export default {
   },
   components : {
     'app-header' : AppHeader
+  }, 
+  methods : {
+    renewStr : function() {
+      this.str = 'hi';
+    }
   }
 }
 </script>
