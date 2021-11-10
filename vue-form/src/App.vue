28d090a28343daa7da6b53e494a5d36e3755d4de
wonjinsong <msmk22239@gmail.com>
2021-11-10 11:11:04 -0500

    user input form

---
+++ vue-form/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">ID : </label>
+      <input type="text" id="username">
+    </div>
+    <div>
+      <label for="password">PW : </label>
+      <input type="password" id="password">
+    </div>
+    <button>LOGIN</button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
