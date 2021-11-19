059351637fcf4400f456759a40e8683476f05a2d
wonjinsong <msmk22239@gmail.com>
2021-11-18 22:52:18 -0500

    input component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-      Input
+      <input type="text" v-model="newTodoItem" placeholder="what to do?">
+      <button v-on:click="addTodo">Add</button>
   </div>
 </template>
 
 <script>
 export default {
-
+   data : function() {
+     return {
+       newTodoItem : '',
+     }
+   },
+   methods : {
+     addTodo : function() {
+       console.log(this.newTodoItem);
+       //로컬스토리지에 저장
+       localStorage.setItem(this.newTodoItem, this.newTodoItem);
+       //input창 초기화
+       this.newTodoItem = '';
+     }
+   }
 }
 </script>
 
