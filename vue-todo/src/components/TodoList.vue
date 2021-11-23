a85e8a98624fbfea3598900fbaf8ed69807bd2e2
wonjinsong <msmk22239@gmail.com>
2021-11-22 23:24:25 -0500

    remove todo list

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
      <ul>
-       <li v-for="todoItem in todoItems" v-bind:key="todoItem">1</li>
+       <li v-for="todoItem in todoItems" v-bind:key="todoItem" class="shadow">
        {{ todoItem }}
+       <span class="removeBtn" v-on:click="removeTodo">
+         <i class="fas fa-trash-alt"></i>
+       </span>
+       </li>
      </ul>
   </div>
 </template>
@@ -14,13 +18,18 @@ export default {
       todoItems : []
     }
   },
+  methods : {
+    removeTodo : function() {
+
+    }
+  },
   // 인스턴스가 생성되자마자 실행되는..hook
   created : function() {
     // 로컬스토리지에 있는 데이터를 가져오기
     if(localStorage.length > 0) {
       for(var i =0;  i < localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          // console.log(localStorage.key(i));
+         console.log(localStorage.key(i));
         this.todoItems.push(localStorage.key(i));
         }        
       }
@@ -30,5 +39,36 @@ export default {
 </script>
 
 <style>
-
+ul {
+  list-style-type : none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+.checkBtn {
+  line-height: 45px;
+  color : #62acde;
+  margin-right: 5px;
+}
+.checkBtnCompleted {
+  color : #b3adad;
+}
+.textCompleted {
+  text-decoration: line-through;
+  color : #b3adad;
+}
+.removeBtn {
+  margin-left: auto;
+  color : #de4343;
+}
 </style>
