d76922837a5f42a39e172b4b7f180f03b83b9294
wonjinsong <msmk22239@gmail.com>
2021-11-24 22:11:05 -0500

    JSON parse stringify

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -35,8 +35,11 @@ export default {
     if(localStorage.length > 0) {
       for(var i =0;  i < localStorage.length; i++) {
         if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-         console.log(localStorage.key(i));
-        this.todoItems.push(localStorage.key(i));
+          //  console.log(localStorage.key(i));
+          // this.todoItems.push(localStorage.key(i));
+          JSON.parse(localStorage.getItem(localStorage.key(i)));
+          //setItem에서 JSON.stringify으로 넣어서 string으로 들어갔고
+          // 이제 getItem에선 다시 obj으로 바꿔가져와야 하믄로
         }        
       }
     }
