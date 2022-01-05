56f9b48c606a547defe0c2a0fd603129a608f6de
wonjinsong <msmk22239@gmail.com>
2022-01-04 20:54:41 -0500

    modal component

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -6,14 +6,24 @@
       <span class="addContainer" v-on:click="addTodo">
         <i class="fas fa-plus addBtn"></i>
       </span>
+
+      <Modal v-if="showModal" @close="showModal = false">
+        <!--
+      you can use custom content here to overwrite
+      default content
+    -->
+        <h3 slot="header">custom header</h3>
+      </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
    data : function() {
      return {
        newTodoItem : '',
+       showModal : false,
      }
    },
    methods : {
@@ -34,6 +44,9 @@ export default {
        //input창 초기화
        this.newTodoItem = '';
      }
+   },
+   components : {
+     Modal : Modal
    }
 }
 </script>
